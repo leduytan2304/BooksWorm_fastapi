8bde337efe858a339e5e2c9fd23c9651762345a1
DTan <leduytan2304@gmail.com>
2025-04-16 17:08:04 +0700

    fixing the filter for homepage

--- Bookstore_be/routers/books.py
+++ Bookstore_be/routers/books.py
@@ -1,14 +1,44 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException,Query
 from sqlalchemy.orm import Session
 from schemas import Book
-from crud.book_crud import get_all_books
+from models import Book as BookModel, Discount
 from database import get_db
+from typing import List
+from sqlalchemy.orm import joinedload
+from typing import Optional
 router = APIRouter()
 
 
 @router.get("/books", response_model=list[Book])
+async def get_books(sort_by : Optional[str] = Query('discount_desc', description = 
+                                                          "sort_option: 'price_asc', 'price_desc','discount_asc', 'discount_des' "),
+                    
+                    db: Session = Depends(get_db)):
+    try:
+       query = db.query(BookModel).options(joinedload(BookModel.discounts))
+       if sort_by:
+         if sort_by == 'asc':
+            query = query.order_by(BookModel.book_price.asc())
+         elif sort_by == 'des':
+            query = query.order_by(BookModel.book_price.desc())
+         elif sort_by == 'discount_asc':
+                query = query.join(BookModel.discounts).order_by(Discount.discount_price.asc())
+         elif sort_by == 'final_price_asc':
+                # Order by final price (original price - discount)
+                query = query.join(BookModel.discounts)\
+                    .order_by((BookModel.book_price - Discount.discount_price).asc())
+         else:
+                raise HTTPException(status_code=400, detail="Invalid sort parameter")
+       else:
+                raise HTTPException(status_code=400, detail="Invalid sort parameter")
 
-async def get_books(db: Session = Depends(get_db)):
-    return get_all_books(db)
+       books = query.all()
+       if not books:
+            raise HTTPException(status_code=404, detail="No books found")
+            
+       return books
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
 
    
