b76df3004d86e7a905060383776f83be9792663b
DTan <leduytan2304@gmail.com>
2025-04-18 14:08:13 +0700

    allow CORS

--- Bookstore_be/main.py
+++ Bookstore_be/main.py
@@ -7,9 +7,17 @@ from routers import auth
 from starlette.middleware.sessions import SessionMiddleware
 from fastapi.middleware.cors import CORSMiddleware
 from routers.config import settings
+
+
 app = FastAPI()
 
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  # Your React app's address
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(books.router, prefix="/api", tags=["/books"])
 app.include_router(auth.router)
