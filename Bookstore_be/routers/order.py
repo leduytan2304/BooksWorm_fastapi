5dead056a1b429aa6a181f9318fd51d722f3cf7d
DTan <leduytan2304@gmail.com>
2025-04-27 17:05:41 +0700

    create new record when press place order

--- Bookstore_be/routers/order.py
+++ Bookstore_be/routers/order.py
@@ -1,24 +1,42 @@
+from datetime import datetime
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from database import get_db
-from schemas import OrderSummary, OrderDetail, List
+from schemas import OrderItemCreate
 from models import Order
 from routers.auth import get_current_user
-
+from typing import List, Optional
 router = APIRouter()
 
-@router.get("/orders/me", response_model=List[OrderSummary])
-async def get_my_orders(
-    db: Session = Depends(get_db),
-    current_user = Depends(get_current_user)
-):
-    try:
-        # Query orders for the current user
-        orders = db.query(Order).filter(Order.user_id == current_user.id).all()
+# @router.get("/orders/me", response_model=List[OrderSummary])
+# async def get_my_orders(
+#     db: Session = Depends(get_db),
+#     current_user = Depends(get_current_user)
+# ):
+#     try:
+#         # Query orders for the current user
+#         orders = db.query(Order).filter(Order.user_id == current_user.id).all()
         
-        if not orders:
-            return []
+#         if not orders:
+#             return []
             
-        return orders
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+#         return orders
+#     except Exception as e:
+#         raise HTTPException(status_code=500, detail=str(e))
+
+@router.post("/orders")
+async def create_new_order(
+    order: OrderItemCreate,
+    db: Session = Depends(get_db),
+    current_user = Depends(get_current_user),
+    
+):
+    new_order = Order(
+        user_id=current_user.id,
+        order_date=datetime.now(),
+        order_amount=order.order_amount
+    )
+    db.add(new_order)
+    db.commit()
+    db.refresh(new_order)
+    return new_order
